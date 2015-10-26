b1a59cf7dc4cbe291aea8dda8c35c492dc46c83c
Matti Salokangas <matti.salokangas@ignitionone.com>
2015-10-25 18:18:57 -0700

    Add URL bar and load requests on enter

--- BlocBrowser/ViewController.m
+++ BlocBrowser/ViewController.m
@@ -7,21 +7,69 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface ViewController ()
-
+@interface ViewController () <WKNavigationDelegate, UITextFieldDelegate>
+    @property (nonatomic, strong) WKWebView *webView;
+    @property (nonatomic, strong) UITextField *textField;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)loadView {
+    UIView *mainView = [UIView new];
+    
+    self.webView = [[WKWebView alloc] init];
+    self.webView.navigationDelegate = self;
+    
+    self.textField = [[UITextField alloc] init];
+    self.textField.keyboardType = UIKeyboardTypeURL;
+    self.textField.returnKeyType = UIReturnKeyDone;
+    self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL field");
+    self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
+    self.textField.delegate = self;
+    
+    [mainView addSubview:self.webView];
+    [mainView addSubview:self.textField];
+    self.view = mainView;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+}
+
+- (void) viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    static const CGFloat itemHeight = 50;
+    CGFloat width = CGRectGetWidth(self.view.bounds);
+    CGFloat browserHeight = CGRectGetHeight(self.view.bounds) - itemHeight;
+    
+    self.textField.frame = CGRectMake(0, 0, width, itemHeight);
+    self.webView.frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), width, browserHeight);
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    
+    NSString *URLString = textField.text;
+    
+    NSURL *URL = [NSURL URLWithString: URLString];
+    
+    if (!URL.scheme) {
+        URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", URLString]];
+    }
+    
+    if (URL) {
+        NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+        [self.webView loadRequest:request];
+    }
+    
+    return NO;
 }
 
 @end
