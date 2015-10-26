cff1b02d0572e6b0c3ee8e059757cc55d6076d62
Matti Salokangas <matti.salokangas@ignitionone.com>
2015-10-25 17:23:03 -0700

    Initial project files

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Matti Salokangas on 10/25/15.
+//  Copyright © 2015 Sturdy Nut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
